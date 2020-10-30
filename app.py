acf2226ba6228351bfd108c85283ce80efed147b
Jouni Leino <jouni.leino@siili.com>
2020-10-30 23:04:54 +0100

    Adding V1 of ComparatorApp

--- app.py
+++ app.py
@@ -1,29 +1,52 @@
 #!/usr/bin/env python3.6
-import sys 
+import os
+import sys
+from photoshop import Session
 from PIL import Image, ImageChops
 from PIL.ImageQt import ImageQt
-from PyQt5.QtWidgets import QMainWindow, QApplication, QScrollArea, QAction, QLabel, QShortcut, QHBoxLayout, QVBoxLayout, QWidget, QFileDialog, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QScrollArea, QAction, QLabel, QShortcut, QHBoxLayout, QVBoxLayout, QWidget, QFileDialog, QPushButton, QFileSystemModel, QComboBox, QButtonGroup
 from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import Qt, QSize
+from datetime import datetime
 
-class ComparatorApp(QMainWindow): 
+class ComparatorApp(QMainWindow):
 
     def __init__(self): 
         super().__init__()
 
+        self.activeContext = "Example"
+        self.activeVersionPrev = ""
+        self.activeVersionNext = ""
+        self.versionPrevDir = "Version1"
+        self.versionNextDir = "Version2"
+        self.defaultLayerComp1 = "Layer Comp 1.png"
         self.get_menu()
         self.statusBar()
-
         self.setWindowTitle("ComparatorApp")
-
+        self.load_example()
+        self.create_dropdowns()
+        self.update_context_dropdown()
         self.create_labels()
+        self.update_prev_folder_dropdown()
+        self.update_next_folder_dropdown()
+        self.update_prev_layercomp_dropdown()
+        self.update_next_layercomp_dropdown()
         self.create_buttons()
+        self.create_button_group()
         self.create_buttons_layout_row1()
         self.create_buttons_layout_row2()
         self.create_shortcuts()
+        self.create_pixmap()
+        self.update_window_size()
+        self.create_layout()
+        self.create_widgets()
+        self.create_signals()
 
-        image1path = './image1.jpg'
-        image2path = './image2.jpg'
+    def load_example(self):
+        versionPrevPath = os.path.join(self.getContextDir(), self.versionPrevDir)
+        versionNextPath = os.path.join(self.getContextDir(), self.versionNextDir)
+        image1path = os.path.join(versionPrevPath, self.defaultLayerComp1)
+        image2path = os.path.join(versionNextPath, self.defaultLayerComp1)
 
         self.image1 = None
         self.image2 = None
@@ -37,13 +60,6 @@ class ComparatorApp(QMainWindow):
         self.qim2 = ImageQt(image2)
         self.qim3 = ImageQt(image3)
 
-        self.create_pixmap()
-        self.add_image_to_label()
-        self.update_window_size()
-        self.create_layout()
-        self.create_widgets()
-        self.create_signals()
-
     def prevBtn_pressed(self):
         self.label.setPixmap(self.pixmap1)
 
@@ -51,6 +67,9 @@ class ComparatorApp(QMainWindow):
         self.label.setPixmap(self.pixmap2)
 
     def diffBtn_pressed(self):
+        self.image3 = self.get_difference(self.image1, self.image2)
+        self.qim3 = ImageQt(self.image3)
+        self.pixmap3 = QPixmap.fromImage(self.qim3)
         self.label.setPixmap(self.pixmap3)
 
     def create_signals(self):
@@ -63,13 +82,22 @@ class ComparatorApp(QMainWindow):
         self.shortcut_left.activated.connect(self.prevBtn_pressed)
         self.shortcut_right = QShortcut(Qt.Key_Right, self)
         self.shortcut_right.activated.connect(self.nextBtn_pressed)
-        self.shortcut_down = QShortcut(Qt.Key_Down, self)
-        self.shortcut_down.activated.connect(self.diffBtn_pressed)
+        self.shortcut_enter = QShortcut(Qt.Key_Return, self)
+        self.shortcut_enter.activated.connect(self.diffBtn_pressed)
 
     def create_buttons(self):
         self.prevBtn = QPushButton("Previous", self)
+        self.prevBtn.setCheckable(True)
         self.nextBtn = QPushButton("Next", self)
+        self.nextBtn.setCheckable(True)
         self.diffBtn = QPushButton("Diff", self)
+        self.diffBtn.setCheckable(True)
+
+    def create_button_group(self):
+        self.btnGroup = QButtonGroup()
+        self.btnGroup.addButton(self.prevBtn)
+        self.btnGroup.addButton(self.nextBtn)
+        self.btnGroup.addButton(self.diffBtn)
 
     def create_buttons_layout_row1(self):
         self.btnLayoutRow1 = QHBoxLayout()
@@ -87,28 +115,111 @@ class ComparatorApp(QMainWindow):
     def create_labels(self):
         self.label = QLabel(self)
 
-    def add_image_to_label(self):
-        self.label.setPixmap(self.pixmap1)
-
     def create_pixmap(self):
         self.pixmap1 = QPixmap.fromImage(self.qim1)
         self.pixmap2 = QPixmap.fromImage(self.qim2)
         self.pixmap3 = QPixmap.fromImage(self.qim3)
 
     def get_menu(self):
-        openPreviousAction = QAction("&Open previous", self)
-        openPreviousAction.triggered.connect(self.loadPreviousDelivery)
-
-        openNewAction = QAction("&Open latest", self)
-        openNewAction.triggered.connect(self.loadNewDelivery)
-
+        openPsdAction = QAction("&Extract PSD", self)
+        openPsdAction.triggered.connect(self.extractLayerCompositionsFromPsd)
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('&File')
-        fileMenu.addAction(openPreviousAction)
-        fileMenu.addAction(openNewAction)
+        fileMenu.addAction(openPsdAction)
+
+    def setContext(self):
+        self.activeContext = self.contextDropdown.currentText()
+        self.update_prev_folder_dropdown()
+        self.update_next_folder_dropdown()
+
+    def setVersionPrev(self):
+        self.activeVersionPrev = self.folderDropdownPrevious.currentText()
+        self.versionPrevDir = os.path.join(self.getContextDir(), self.activeVersionPrev)
+        self.update_prev_layercomp_dropdown()
+
+    def setVersionNext(self):
+        self.activeVersionNext = self.folderDropdownNext.currentText()
+        self.versionNextDir = os.path.join(self.getContextDir(), self.activeVersionNext)
+        self.update_next_layercomp_dropdown()
+
+    def setLayerCompPrev(self):
+        self.activeLayerCompPrev = self.layerCompsDropdownPrevious.currentText()
+        if self.activeLayerCompPrev:
+            imagePath1 = os.path.join(self.versionPrevDir, self.activeLayerCompPrev)
+            self.image1 = Image.open(imagePath1).convert('RGB')
+            self.pixmap1 = QPixmap(imagePath1)
+            self.label.setPixmap(self.pixmap1)
+
+    def setLayerCompNext(self):
+        self.activeLayerCompNext = self.layerCompsDropdownNext.currentText()
+        if self.activeLayerCompNext:
+            imagePath2 = os.path.join(self.versionNextDir, self.activeLayerCompNext)
+            self.image2 = Image.open(imagePath2).convert('RGB')
+            self.pixmap2 = QPixmap(imagePath2)
+            self.label.setPixmap(self.pixmap2)
+
+    def update_context_dropdown(self):
+        dirs = [name for name in os.listdir(".") if os.path.isdir(name)]
+        self.contextDropdown.clear()
+        self.contextDropdown.addItems(dirs)
+
+    def create_dropdowns(self):
+        self.contextDropdown = QComboBox()
+        self.contextDropdown.currentIndexChanged.connect(self.setContext)
+        self.folderDropdownPrevious = QComboBox()
+        self.folderDropdownPrevious.currentIndexChanged.connect(self.setVersionPrev)
+        self.folderDropdownNext = QComboBox()
+        self.folderDropdownNext.currentIndexChanged.connect(self.setVersionNext)
+        self.layerCompsDropdownPrevious = QComboBox()
+        self.layerCompsDropdownPrevious.currentIndexChanged.connect(self.setLayerCompPrev)
+        self.layerCompsDropdownNext = QComboBox()
+        self.layerCompsDropdownNext.currentIndexChanged.connect(self.setLayerCompNext)
+        self.folderDropdownLayout = QHBoxLayout()
+        self.folderDropdownLayout.addWidget(self.folderDropdownPrevious)
+        self.folderDropdownLayout.addWidget(self.folderDropdownNext)
+        self.layerCompsDropdownLayout = QHBoxLayout()
+        self.layerCompsDropdownLayout.addWidget(self.layerCompsDropdownPrevious)
+        self.layerCompsDropdownLayout.addWidget(self.layerCompsDropdownNext)
+
+    def update_prev_folder_dropdown(self):
+        if self.activeContext:
+            dirs = [name for name in os.listdir(self.activeContext)]
+            self.folderDropdownPrevious.clear()
+            self.folderDropdownPrevious.addItems(dirs)
+            self.folderDropdownPrevious.setCurrentIndex(0)
+
+    def update_next_folder_dropdown(self):
+        if self.activeContext:
+            dirs = [name for name in os.listdir(self.activeContext)]
+            self.folderDropdownNext.clear()
+            self.folderDropdownNext.addItems(dirs)
+            self.folderDropdownNext.setCurrentIndex(1)
+
+    def update_prev_layercomp_dropdown(self):
+        layerComps = []
+        self.layerCompsDropdownPrevious.clear()
+        if self.activeVersionPrev:
+            dirPath = os.path.join(os.getcwd(), self.activeContext)
+            dirPath = os.path.join(dirPath, self.activeVersionPrev)
+            for layerComp in os.listdir(dirPath):
+                layerComps.append(layerComp)
+            self.layerCompsDropdownPrevious.addItems(layerComps)
+
+    def update_next_layercomp_dropdown(self):
+        layerComps = []
+        self.layerCompsDropdownNext.clear()
+        if self.activeVersionNext:
+            dirPath = os.path.join(os.getcwd(), self.activeContext)
+            dirPath = os.path.join(dirPath, self.activeVersionNext)
+            for layerComp in os.listdir(dirPath):
+                layerComps.append(layerComp)
+            self.layerCompsDropdownNext.addItems(layerComps)
 
     def create_layout(self):
         self.layout = QVBoxLayout()
+        self.layout.addWidget(self.contextDropdown)
+        self.layout.addLayout(self.folderDropdownLayout)
+        self.layout.addLayout(self.layerCompsDropdownLayout)
         self.layout.addLayout(self.btnLayoutRow1)
         self.layout.addLayout(self.btnLayoutRow2)
         self.layout.addWidget(self.label)
@@ -134,33 +245,34 @@ class ComparatorApp(QMainWindow):
     def update_window_size(self):
         self.setGeometry(0, 0, self.pixmap1.width(), self.pixmap1.height() + 100)
 
-    def loadPreviousDelivery(self):
+    def extractLayerCompositionsFromPsd(self):
         fileName = self.openFileDialog()
         if fileName:
-            print(fileName)
-            self.image1 = Image.open(fileName).convert('RGB')
-            self.pixmap1 = QPixmap(fileName)
-            self.pixmap2 = QPixmap()
-            self.pixmap3 = QPixmap()
-            self.label.setPixmap(self.pixmap1)
+            print("Start extracting")
+            with Session(fileName, action="open") as ps:
+                fileName = os.path.basename(fileName)
+                print("Opening PSD file", fileName)
+                fileName , extension = os.path.splitext(fileName)
+                if not os.path.exists(os.path.join(os.getcwd(), self.activeContext)):
+                    os.mkdir(self.activeContext)
+                extractDir = os.path.join(self.getContextDir(), fileName + "_" + datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+                os.mkdir(extractDir)
+                doc = ps.active_document
+                options = ps.PNGSaveOptions()
+                for layerComp in doc.layerComps:
+                    layerComp.apply()
+                    extractPath = os.path.join(extractDir, f"{layerComp.name}.png")
+                    print("Extract ", extractPath)
+                    doc.saveAs(extractPath, options, True)
+            print("Finished extracting")
 
-    def loadNewDelivery(self):
-        fileName = self.openFileDialog()
-        if fileName:
-            print(fileName)
-            self.image2 = Image.open(fileName).convert('RGB')
-            self.pixmap2 = QPixmap(fileName)
-            self.update_diff()
-
-    def update_diff(self):
-        self.image3 = self.get_difference(self.image1, self.image2)
-        self.qim3 = ImageQt(self.image3)
-        self.pixmap3 = QPixmap.fromImage(self.qim3)
+    def getContextDir(self):
+        cwd = os.getcwd()
+        return os.path.join(cwd, self.activeContext)
 
     def openFileDialog(self):    
         options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "", filter="*.png *.jpg", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "", filter="*.png *.psd", options=options)
         return fileName
 
     def closeApplication(self):
